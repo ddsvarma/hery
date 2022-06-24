76b925c5e4f25e50c0c1051a256dfaddc80de103
ddsvarma <contactddsvarma@gmail.com>
2022-06-24 13:26:03 +0530

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ChildComponentComponent } from './components/child-component/child-component.component';
+import { ParentComponentComponent } from './components/parent-component/parent-component.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, FlexLayoutModule],
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    ParentComponentComponent,
+    ChildComponentComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/parent-component/parent-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'parent-component',
+  templateUrl: './parent-component.component.html',
+  styleUrls: ['./parent-component.component.css'],
+})
+export class ParentComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
